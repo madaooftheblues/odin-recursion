64ac2c5ad1be83794499962bda74adbe764b1dcd
madaooftheblues <azlanrasool@gmail.com>
2024-02-17 14:40:07 +0500

    Improve efficiency in fibsRec function

--- fibonacci-sequence.js
+++ fibonacci-sequence.js
@@ -14,5 +14,10 @@ function fibsRec(n) {
     if (n === 1) return [0]
     if (n === 2) return [0, 1]
 
-    return [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]]
+    const prevFibs = fibsRec(n - 1)
+    const nthFib = prevFibs[n - 2] + prevFibs[n - 3]
+
+    return [...prevFibs, nthFib]
 }
+
+console.log(fibsRec(8))
